3cc51ccd7ccdaf169ecbae982d178926ef9b18a5
efedotdev <bilgili.dev0@gmail.com>
2026-02-18 22:14:51 +0300

    valifation tamam yarın aspectler bitecek

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(P => P.ProductName).NotEmpty();
+            RuleFor(P => P.ProductName).MinimumLength(2);
+            RuleFor(P => P.UnitPrice).NotEmpty();
+            RuleFor(P => P.UnitPrice).GreaterThan(0);
+            RuleFor(P => P.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            RuleFor(P => P.ProductName).Must(StartWhitA).WithMessage("Product name must start with A"); // custom validation
+        }
+        private bool StartWhitA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}

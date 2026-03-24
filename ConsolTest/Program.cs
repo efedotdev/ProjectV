e2134d39cbc3863947e587fc407178deca6b387c
efedotdev <bilgili.dev0@gmail.com>
2026-03-25 00:31:31 +0300

    25.03.26

--- ConsolTest/Program.cs
+++ ConsolTest/Program.cs
@@ -10,10 +10,10 @@ namespace ConsolTest
     {
         static void Main(string[] args)
         {
-            ProductManager p = new ProductManager(new EfProductDal());
-            Product phone = new Product() { ProductId=9,ProductName = "Iphone 14", CategoryId = 1, IsActive = true, UnitPrice = 55000, UnitsInStock =3 , ProductCode = "ELK-004" };
-            Console.WriteLine("Sistem başlatıldı.");
-            Console.WriteLine(p.Update(phone).Message);
+            //ProductManager p = new ProductManager(new EfProductDal());
+            //Product phone = new Product() { ProductId=9,ProductName = "Iphone 14", CategoryId = 1, IsActive = true, UnitPrice = 55000, UnitsInStock =3 , ProductCode = "ELK-004" };
+            //Console.WriteLine("Sistem başlatıldı.");
+            //Console.WriteLine(p.Update(phone).Message);
             
         }
     }

cd926a0f350ba26fec1b625a56ca46809c040f85
efedotdev <bilgili.dev0@gmail.com>
2026-04-17 18:30:35 +0300

    log

--- WebAPI/Extensions/SerilogExtensions.cs
+++ WebAPI/Extensions/SerilogExtensions.cs
@@ -9,7 +9,13 @@ namespace WebAPI.Extensions
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Information()
                 .WriteTo.Console()
-                .WriteTo.Async(a => a.File("Logs/log-.txt", rollingInterval: RollingInterval.Day))
+                .WriteTo.Async(a => a.File(
+                   path: "Logs/log-.txt",
+                   rollingInterval: RollingInterval.Day,
+                   retainedFileCountLimit: 15,
+                   fileSizeLimitBytes: 10 * 1024 * 1024,
+                   rollOnFileSizeLimit: true
+                ))
                 .CreateLogger();
 
             builder.Host.UseSerilog();
